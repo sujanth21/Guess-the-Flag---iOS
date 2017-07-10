3a076184338e4196007e615b6f6683f87993b584
Sujanth <sujanth1987@gmail.com>
2017-07-10 19:58:25 +1000

    Added GamePlayKit for randomize buttons

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
 //
 
+import GameplayKit
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
     var score = 0
     
     override func viewDidLoad() {
@@ -35,12 +37,27 @@ class ViewController: UIViewController {
         countries.append("us")
         
         askQuestion()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
     }
     
     func askQuestion() {
+        
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        
+        title = countries[correctAnswer].uppercased()
     }
 
     override func didReceiveMemoryWarning() {
