093e902e60fe5d5c9417faa59d65f6e053e9cd45
Sujanth <sujanth1987@gmail.com>
2017-07-10 19:31:48 +1000

    Added askQuestion method

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -11,14 +11,36 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var button1: UIButton!
-    
     @IBOutlet weak var button2: UIButton!
-    
     @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
