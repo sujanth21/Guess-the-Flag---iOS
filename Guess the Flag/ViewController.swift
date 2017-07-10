0a219f01e342a39ac3a5f0e46370f671f9c467bf
Sujanth <sujanth1987@gmail.com>
2017-07-10 19:13:33 +1000

    Fixed UI constrained

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    @IBOutlet weak var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
